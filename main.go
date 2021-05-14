664bbef786a4d60c7edbc2fa46c916615d9a0b9d
sahilthakur7 <sahilrd07@gmail.com>
2021-05-14 17:34:02 +0530

    Serving index file

--- main.go
+++ main.go
@@ -7,11 +7,22 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func newRouter() *mux.Router {
+	r:= mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	staticFileDirectory := http.Dir("./assets")
+
+	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+
+	return r
+}
+
 func main() {
 
-	router := mux.NewRouter()
-
-	router.HandleFunc("/", handler).Methods("GET")
+	router := newRouter()
 
 	http.ListenAndServe(":8080", router)
 }
---
+++ main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	req, err := http.NewRequest("GET","", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	hf := http.HandlerFunc(handler)
+	hf.ServeHTTP(recorder, req)
+
+	// Check the status code is what we expect.
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := `Hello World!`
+	actual := recorder.Body.String()
+	if actual != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", actual, expected)
+	}
+}
